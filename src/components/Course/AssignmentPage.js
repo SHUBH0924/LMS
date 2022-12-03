f5beb0390502e31df70b2e7756a1060ed8724f5f
shuklaashvani <ashvanishukla234@gmail.com>
2022-12-03 12:37:42 +0530

    up

--- src/components/Course/AssignmentPage.js
+++ src/components/Course/AssignmentPage.js
@@ -5,10 +5,12 @@ import PDFViewer from '../Admin/AdminDashboard/CreateCourse/Course_Modules/Page/
 import axios from 'axios';
 import { useAuth } from '../../Auth/auth';
 import Header from '../Header'
+import { Input } from 'postcss';
 
 const AssignmentPage = (props) => {
 
   const auth = useAuth()
+  const userRole =  auth.user;
   const token = auth.token
   const location = useLocation();
   // const lec = location.state.assignmentDetail
@@ -19,6 +21,7 @@ const AssignmentPage = (props) => {
 
   const [content, setContent] = useState(location.state.content)
   const [Title, setTitle] = useState(location.state.Title)
+  
 
   return (
     <>
@@ -27,17 +30,20 @@ const AssignmentPage = (props) => {
         <Header />
       </div>
       <div className=' flex flex-row w-full overflow-x-hidden h-screen bg-white'>
-
+        
         <div className=' flex flex-col w-full  mx-auto ' >
 
           <h1 className='text-4xl mx-auto mt-6 font-bold text-black '>
             {Title}
           </h1>
           <hr className='mt-8 w-3/5 mb-8 mx-auto' />
+          
           <div 
             className='ml-12  mr-12 mb-8'
             dangerouslySetInnerHTML={{ __html: content }} 
           />
+          
+
 
           {
             (hasFile) ? (
@@ -45,6 +51,17 @@ const AssignmentPage = (props) => {
                 <PDFViewer url={`assignment/file/${assignmentId}/${token}`}/>
               </div>) : null
           }
+          
+          <div>
+              <div className='mt-8'>
+                <span>
+                <label className="appearance-none w-36 block mx-auto block uppercase tracking-wide text-gray-700 text-xl font-bold mb-2">
+                 Upload Assignment  <input className='mx-auto mb-5'  type="file"  placeholder='Upload Assignment'/>
+                </label>
+                  <button className="appearance-none w-36 block mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" id="update" type="file" >Upload</button>
+                  </span>
+              </div>
+          </div>
           
         </div>
       </div>
